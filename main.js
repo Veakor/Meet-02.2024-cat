084099764b4a0a939f060b82dfc107f204be9ba3
Veakor <135655247+Veakor@users.noreply.github.com>
2024-02-07 22:42:05 +0100

    add

--- main.js
+++ main.js
@@ -4,13 +4,18 @@ document.addEventListener('DOMContentLoaded', function() {
     const catImage = document.getElementById('catImage');
 
     okButton.addEventListener('click', function() {
-        catImage.src = 'smiling_cat.gif'; // Изменение изображения на улыбающегося котенка
+        catImage.src = 'smiling_cat.gif';
+        const catImage = new Image();
+    catImage.src = 'http://hoppip.tumblr.com/post/27010295505/fluffy';
+    catImage.alt = 'Fluffy Cat';
+    catContainer.appendChild(catImage); 
     });
 
     noButton.addEventListener('mouseenter', function() {
-        const randomX = Math.random() * (window.innerWidth - 200); // Случайная позиция по оси X
-        const randomY = Math.random() * (window.innerHeight - 200); // Случайная позиция по оси Y
-        noButton.style.left = randomX + 'px'; // Установка позиции по оси X
-        noButton.style.top = randomY + 'px'; // Установка позиции по оси Y
+        const randomX = Math.random() * (window.innerWidth - 600); 
+        const randomY = Math.random() * (window.innerHeight - 600);
+        noButton.style.position = 'absolute'; 
+        noButton.style.left = randomX + 'px'; 
+        noButton.style.top = randomY + 'px'; 
     });
 });
